f5715543401ebdc9dff1b5d7559ed67d407adc94
Austin Ziegler <austin@zieglers.ca>
2023-02-17 23:58:00 -0500

    Release 3.2023.0218

--- lib/mime/types/data.rb
+++ lib/mime/types/data.rb
@@ -3,7 +3,7 @@
 module MIME
   class Types
     module Data
-      VERSION = "3.2022.0105"
+      VERSION = "3.2023.0218"
 
       # The path that will be used for loading the MIME::Types data. The
       # default location is __FILE__/../../../../data, which is where the data
