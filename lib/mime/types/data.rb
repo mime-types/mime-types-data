d8cf731784c2e054f635a583a81b2ad2487152ee
halostatue <11361+halostatue@users.noreply.github.com>
2024-04-02 18:10:26 +0000

    Update mime-types-data 3.2024.0402 / 2024-04-02
    - Updated the Apache and IANA media registry entries as of release date

--- lib/mime/types/data.rb
+++ lib/mime/types/data.rb
@@ -3,7 +3,7 @@
 module MIME
   class Types
     module Data
-      VERSION = "3.2024.0305"
+      VERSION = "3.2024.0402"
 
       # The path that will be used for loading the MIME::Types data. The
       # default location is __FILE__/../../../../data, which is where the data
